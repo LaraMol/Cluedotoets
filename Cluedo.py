771b269eb77986d8ae7ee4c635e69566a03e98cb
Lara Mol <89733492+LaraMol@users.noreply.github.com>
2021-09-27 14:05:22 +0200

    Add files via upload

---
+++ Cluedo.py
@@ -0,0 +1,26 @@
+
+while True:
+    sollicitatie = input ("Bent u klaar voor de sollicitatie over cluedo? ")
+    if sollicitatie == "ja":
+        break
+    elif sollicitatie == "nee":
+        print ("Wat doe je hier dan")
+    else:
+        print ("Invalid option")
+
+print ("\nSollicitatie wordt ingeladen.")
+
+naam = input ("Hallo sollicitant type hier uw naam ")
+leeftijd = input ("Hoe oud bent u? ")
+
+geslacht = input ("Wat is uw geslacht? ")
+if geslacht == "vrouw":
+    print ("Antwoord opgeslagen")
+        diploma = input ("Heeft u een acteurs diploma? ")
+            if diploma == "ja":
+                print ("Antwoord opgeslagen")
+            elif diploma == "nee":
+elif geslacht == "man":
+    print ("Antwoord opgeslagen")
+else:
+    print ("Dat hebben we niet")
