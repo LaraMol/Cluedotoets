9be0bec75b42c07701760a127767b0f8d7af0de0
Lara Mol <89733492+LaraMol@users.noreply.github.com>
2021-09-28 22:32:00 +0200

    Add files via upload

--- Cluedo2.py
+++ Cluedo2.py
@@ -1,30 +1,50 @@
+
 while True:
     sollicitatie = input("Bent u klaar voor de sollicitatie? ")
     if sollicitatie == "ja":
         break
     else:
         print("Wat doe je hier dan? ")
-
-print ("Sollicitatie wordt ingeladen.") 
 print ("----------------------------------------------------------------")
+naam = input ("Wat is uw naam? ")
 
-naam = input ("Beste sollicitant hoe heet u? ")
-leeftijd = input ("Wat is uw leeftijd? ")
 
-Geslacht = input ("Bent u een man of een vrouw? ")
-if Geslacht == "vrouw":
-    diploma = input ("Heeft u een acteurs diploma? ")
-    if diploma == "ja":
-        print ("Antwoord opgeslagen")
-        oud = input ("Staat u bereid om een ouder character te acteren?")
-        if oud == "ja":
-            print ("Antwoord opgeslagen")
-        elif oud == "nee":
-            print ("Antwoord opgeslagen")
-            
-elif Geslacht == "man":
-    diploma = input ("Heeft u een acteurs diploma? ")
-    if diploma == "nee":
-        print("Antwoord opgeslagen")
-    
+geslacht = input ("\nBent u een man of vrouw? ")
+leeftijd = int(input("\nHoe oud bent u? "))
+diploma = input ("\nHeeft u een acteurs diploma? ")
+oud = input ("\nBent u bereid om een ouder personage te spelen? ")
+koken = input ("Kunt u koken? ")
+snor = input ("\nKunt u een snor laten groeien? ")
+haar = input ("\nZit u meer aan de blond, bruin of grijze kant qua haar? ")
+verf = input ("\nVind u het erg om uw haar te verven / te scheren? ")
+bril = input ("\nHeeft u een bril? ")
+persoonlijkheid = input ("\nBent u meer een chique persoon of meer een ouderwetse persoon? ")
+feesten = input("\nVind u het leuk om naar feesten te gaan? ")
+kringen = input ("\nHeeft u donkere kringen onder uw ogen? ")
+leger = input ("\nHeeft u ervaring in het leger? ")
+wapens = input ("\nBent u gespecialiseerd in wapens? ")
+
+if leeftijd >= 18:
+    if geslacht == "vrouw" and diploma == "ja":
+        if koken == "ja" and haar == "grijze" and oud == "ja":
+            print ("U kunt op auditie voor Mw. De Wit.")
+        elif persoonlijkheid == "ouderwets" and haar == "blond" and feesten == "ja":
+            print ("U kunt op auditie voor Mw. Blaauw Van Draet.")
+        elif persoonlijkheid == "chique" and haar == "bruin" and kringen == "ja":
+            print ("U kunt op auditie voor Rosa Roodhart.")
+        else:
+            print ("Je mag helaas niet op auditie.")
+
+    if geslacht == "man" and diploma == "ja":
+        if leger == "ja" and haar == "grijze" and wapens == "ja":
+            print ("U kunt op auditie voor Kolonel van Geelen.")
+        elif oud == "ja" and bril == "ja" and snor == "ja":
+            print ("U kunt op auditie voor Professor Pimpel.")
+        elif oud == "ja" and verf == "ja" and feesten == "nee":
+            print ("U kunt op auditie voor Dominee Groenenwoud.")
+        else:
+            print("Je mag helaas niet op auditie.")
+
+elif leeftijd < 18 and diploma == "nee":
+    print ("Je mag helaas niet op auditie.")
 
